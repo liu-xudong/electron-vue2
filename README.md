98c2855701f1d93f51b6bb420d9f001700585059
liu-xudong <z17609200438@163.com>
2021-12-01 15:52:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# electron-vue2
+
+## dist-electron
+
+​	打包客户端文件位置
+
+## electron
+
+​	主进程程序位置
+
+## 命令
+
+```javascript
+// 启动程序
+yarn electron:serve
+npm run electron:serve
+
+// 打包程序
+yarn electron:build
+npm run electron:build
+```
+
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="chrome-version"></div>
+    <div id="node-version"></div>
+    <div id="electron-version"></div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
